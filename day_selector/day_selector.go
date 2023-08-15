24073ffa756629d48e488b6be5f3756ea0af18c1
luckponte <luckpontecorreia@gmail.com>
2023-08-15 14:44:39 +0100

    Add month dependency to random day selector

--- day_selector/day_selector.go
+++ day_selector/day_selector.go
@@ -6,10 +6,27 @@ type IRandomDaySelector interface {
 	randomDay() int
 }
 
-type randomDaySelector struct{}
+type randomDaySelector struct {
+	dayLimit int
+}
+
+var monthLimits = map[int]int{
+	1:  31,
+	2:  28,
+	3:  31,
+	4:  30,
+	5:  31,
+	6:  30,
+	7:  31,
+	8:  31,
+	9:  30,
+	10: 31,
+	11: 30,
+	12: 31,
+}
 
 func (r *randomDaySelector) randomDay() int {
-	return rand.Intn(30) + 1
+	return rand.Intn(r.dayLimit) + 1
 }
 
 func selectDays(numDays int, rd IRandomDaySelector) []int {
@@ -21,6 +38,7 @@ func selectDays(numDays int, rd IRandomDaySelector) []int {
 	return selectedDays
 }
 
-func SelectDays(numDays int) []int {
-	return selectDays(numDays, &randomDaySelector{})
+func SelectDays(numDays int, month int) []int {
+	daySelector := randomDaySelector{monthLimits[month]}
+	return selectDays(numDays, &daySelector)
 }
--- day_selector/day_selector_test.go
+++ day_selector/day_selector_test.go
@@ -18,7 +18,7 @@ func TestSelectDay(t *testing.T) {
 		t.Run(fmt.Sprintf("should select %d days", rune(testCase.want)), func(t *testing.T) {
 			t.Helper()
 
-			got := SelectDays(testCase.want)
+			got := SelectDays(testCase.want, 1)
 
 			if len(got) != testCase.want {
 				t.Errorf("Wanted %d length, got %d", testCase.want, len(got))
@@ -30,12 +30,44 @@ func TestSelectDay(t *testing.T) {
 	t.Run("numbers should be month days", func(t *testing.T) {
 		t.Helper()
 
-		got := SelectDays(100)
+		testCases := []struct {
+			month    int
+			dayLimit int
+		}{
+			{1, 31},
+			{2, 28},
+			{3, 31},
+			{4, 30},
+			{5, 31},
+			{6, 30},
+			{7, 31},
+			{8, 31},
+			{9, 30},
+			{10, 31},
+			{11, 30},
+			{12, 31},
+		}
 
-		for i := 0; i < 100; i++ {
-			if got[i] > 31 || got[i] < 1 {
-				t.Errorf("Wanted number between 1-31, got %d (array: %v)", got[i], got)
-			}
+		for _, testCase := range testCases {
+			t.Run(fmt.Sprintf("Month %d", testCase.month), func(t *testing.T) {
+				t.Helper()
+				got := SelectDays(1000, testCase.month)
+				reachedLimit := false
+
+				for i := 0; i < 1000; i++ {
+					if got[i] > testCase.dayLimit || got[i] < 1 {
+						t.Errorf("Wanted number between 1-%d, got %d (array: %v)", testCase.dayLimit, got[i], got)
+					}
+
+					if got[i] == testCase.dayLimit {
+						reachedLimit = true
+					}
+				}
+
+				if !reachedLimit {
+					t.Errorf("Expected to reach day %d at least once", testCase.dayLimit)
+				}
+			})
 		}
 	})
 
